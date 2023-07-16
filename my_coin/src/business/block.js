8d1b5bdd62b51a548852b1771fad2afe15b3c69e
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-07-16 18:01:21 +0700

    Feat: Create mining rewards and transactions

--- my_coin/src/business/block.js
+++ my_coin/src/business/block.js
@@ -1,18 +1,24 @@
 const SHA256 = require("crypto-js/sha256");
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = ""){
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ""){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
-    }
+        return SHA256(this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();    }
 
     mineBlock(difficulty) {
         while(this.hash.substring(0, difficulty) != Array(difficulty +1).join("0")) { // Trick to test whether hash start with enough "0" length or not
@@ -21,29 +27,60 @@ class Block {
         }
 
         console.log("Block mined: " + this.hash);
-        console.log(this.nonce);
+        // console.log(this.nonce);
     }
 }
 
 class BlockChain {
     constructor() {
         this.chain = [this.createFirstBlock()];
-        this.difficulty = 4;
-    }
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 10;    }
 
     createFirstBlock() {
-        return new Block(0, "14/07/2023", "Genesis Block", 0);
+        return new Block("15/07/2023", "Genesis Block", 0);
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        // newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    // addBlock(newBlock) {
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     // newBlock.hash = newBlock.calculateHash();
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+
+    minePendingTransaction (miningRewardAddress) {
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("Successfully mined...");
+        this.chain.push(block);
+
+        // Reset pending transaction array, and create new mining transation to give miner rewards, reciever is miner, sender can be null
+        this.pendingTransactions = [new Transaction(null, miningRewardAddress, this.miningReward)];
+    }
+
+    createTransaction (transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAnAddress(address) {
+        let balance = 0;
+        for (const block of this.chain){
+            for (const tran of block.transactions){
+                if (tran.fromAddress == address){
+                    balance -= tran.amount;
+                }
+                if (tran.toAddress == address){
+                    balance += tran.amount;
+                }
+            }
+        }
+        return balance;
     }
 
     isChainValid() {
@@ -64,11 +101,17 @@ class BlockChain {
 }
 
 let myCoin = new BlockChain();
-console.log("Mining block 1: ");
+myCoin.createTransaction(new Transaction("address1", "address2", 100)); // Address is a public key of a wallet
+myCoin.createTransaction(new Transaction("address2", "address1", 50));
 
-myCoin.addBlock(new Block(1, "14/07/2023", { amount: 10}));
-console.log("Mining block 2: ");
-myCoin.addBlock(new Block(2, "14/07/2023", { amount: 20}));
+console.log("Start mining...");
+myCoin.minePendingTransaction("myaddress");
+console.log("Balance of my address is: " + myCoin.getBalanceOfAnAddress("myaddress"));
 
-console.log(JSON.stringify(myCoin, null, 4));
-console.log("Is chain valid? " + myCoin.isChainValid());
+console.log("Start mining...");
+myCoin.minePendingTransaction("myaddress");
+console.log("Balance of my address is: " + myCoin.getBalanceOfAnAddress("myaddress"));
+
+console.log("Start mining...");
+myCoin.minePendingTransaction("myaddress");
+console.log("Balance of my address is: " + myCoin.getBalanceOfAnAddress("myaddress"));
