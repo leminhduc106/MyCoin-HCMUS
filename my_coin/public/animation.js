de7400c630c6676c4a3a0124dcee8623fca06357
minhduc <64589567+leminhduc106@users.noreply.github.com>
2023-07-18 18:35:03 +0700

    Feat: add wallet history

--- my_coin/public/animation.js
+++ my_coin/public/animation.js
@@ -2,31 +2,33 @@ document.addEventListener("DOMContentLoaded", function(){
     
     // 
     const sendcoin = document.getElementsByClassName('btnsendcoid')[0];
-    var sendcoinform = document.getElementsByClassName('card-send-coin')[0];
+    let sendcoinform = document.getElementsByClassName('card-send-coin')[0];
     const exitsendcoin = document.getElementsByClassName('exitsendcoin')[0];
 
     const btnview = document.getElementsByClassName('btnview')[0];
-    var cardview = document.getElementsByClassName('card-view')[0];
+    let cardview = document.getElementsByClassName('card-view')[0];
     const iconexit = document.getElementsByClassName('iconexit')[0];
+
+    const btnviewhistory = document.getElementsByClassName('btnviewhistory')[0];
+    let cardviewhistory = document.getElementsByClassName('card-view-history')[0];
+    const iconexithistory = document.getElementsByClassName('iconexithistory')[0];
 
     const btncreatewallet = document.getElementsByClassName('btncreatewallet')[0];
     const btnchangewallet = document.getElementsByClassName('btnchangewallet')[0];
     const btnformcreatewallet = document.getElementsByClassName('btnformcreatewallet')[0];
 
-    var cardcreatewallet = document.getElementsByClassName('cardcreatewallet')[0];
-    var cardchangewallet = document.getElementsByClassName('cardchangewallet')[0];
+    let cardcreatewallet = document.getElementsByClassName('cardcreatewallet')[0];
+    let cardchangewallet = document.getElementsByClassName('cardchangewallet')[0];
 
-    var formcreatewallet = document.getElementsByClassName('formcreatewallet')[0];
-    formcreatewallet.onclick = () =>{
+ let formcreatewallet = document.getElementsByClassName('formcreatewallet')[0];    formcreatewallet.onclick = () =>{
         formcreatewallet.reset();
     }
-
-
 
      // button click send coin
     sendcoin.onclick = () =>{
     	sendcoinform.classList.toggle('xuathien');
         cardview.classList.remove('xuathien');
+        cardviewhistory.classList.remove('xuathien');
         cardchangewallet.classList.remove('xuathien');
         cardcreatewallet.classList.remove('xuathien');
         formcreatewallet.reset();
@@ -40,12 +42,10 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
 
-
-    // button click view statistical
-
     btnview.onclick = () =>{
     	cardview.classList.toggle('xuathien');
         sendcoinform.classList.remove('xuathien');
+        cardviewhistory.classList.remove('xuathien');
         cardchangewallet.classList.remove('xuathien');
         cardcreatewallet.classList.remove('xuathien');
         formcreatewallet.reset();
@@ -55,11 +55,26 @@ document.addEventListener("DOMContentLoaded", function(){
     	cardview.classList.remove('xuathien');
     }
 
+
+    // button click view history
+    btnviewhistory.onclick = () =>{
+    	cardviewhistory.classList.toggle('xuathien');
+        cardview.classList.remove('xuathien');
+        sendcoinform.classList.remove('xuathien');
+        cardchangewallet.classList.remove('xuathien');
+        cardcreatewallet.classList.remove('xuathien');
+        formcreatewallet.reset();
+        document.getElementById('formsendcoin').reset();
+    }
+    iconexithistory.onclick = () =>{
+    	cardviewhistory.classList.remove('xuathien');
+    }
     
     
     btncreatewallet.onclick = () =>{
     	cardcreatewallet.classList.toggle('xuathien');
         cardchangewallet.classList.remove('xuathien');
+        cardviewhistory.classList.remove('xuathien');
         cardview.classList.remove('xuathien');
         sendcoinform.classList.remove('xuathien');
         document.getElementById('formsendcoin').reset();
@@ -68,6 +83,7 @@ document.addEventListener("DOMContentLoaded", function(){
     btnchangewallet.onclick = () =>{
     	cardchangewallet.classList.toggle('xuathien');
         cardcreatewallet.classList.remove('xuathien');
+        cardviewhistory.classList.remove('xuathien');
         cardview.classList.remove('xuathien');
         sendcoinform.classList.remove('xuathien');
         formcreatewallet.reset();
